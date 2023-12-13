d98c4885db6e7885644cbd744a437071f9bb2eea
kylestech95 <kylestech95@outlook.com>
2023-12-13 12:54:35 -0500

    Menu

--- lemonade-stand/lemonade.sh
+++ lemonade-stand/lemonade.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
-echo -e "\n~~~ Welcome to my Lemonade Stand ~~~\n"
 # Declare PSQL
 PSQL="psql -X --username=postgres --dbname=lemonade --tuples-only -c"
 
 # Uncomment truncate next line to refresh database values
 # echo $($PSQL "truncate product,transaction,customers")
 
-
-
-
+echo -e "\n~~~ Welcome to my Lemonade Stand ~~~"
 
 MENU(){
-echo -e "\nMain Menu\n?"
-echo -e "\n1)Purchase lemons\n2)view our sales\n3)exit"
+if [[ $1 ]]
+then echo -e "\n$1"
+fi
+echo -e "Main Menu\n"
+echo -e "1)Purchase lemons\n2)view our sales\n3)exit"
+read OPTION
+
+case $OPTION in 
+1) PURCHASE ;;
+2) VIEW_OUR_SALES ;;
+3) EXIT ;;
+esac
+
 }
 PURCHASE(){
-echo -e "\nWelcome to Purchase"
+echo -e "Welcome to Purchase"
 }
 VIEW_OUR_SALES(){
-echo -e "\nView our sales"
+echo -e "View our sales"
 }
+EXIT(){
+    sleep 1
+    MENU
+}
+
+
+MENU
