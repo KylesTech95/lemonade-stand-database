32eea827112aece73c1c431556e259363a219775
kylestech95 <kylestech95@outlook.com>
2023-12-14 16:19:44 -0500

    slowly but surely

--- lemonade-stand/lemonade.sh
+++ lemonade-stand/lemonade.sh
@@ -5,25 +5,8 @@ PSQL="psql -X --username=postgres --dbname=lemonade --tuples-only -c"
 
 echo -e "\n~~~ Welcome to my Lemonade Stand ~~~"
 
-MENU(){
-if [[ $1 ]]
-then echo -e "\n$1"
-fi
-echo -e "Main Menu\n"
-echo -e "1)Purchase lemons\n2)view our sales\n3)exit"
-read OPTION
-
-case $OPTION in 
-1) PURCHASE ;;
-2) VIEW_OUR_SALES ;;
-3) EXIT ;;
-esac
-
-}
-PURCHASE(){
-echo -e "Welcome to Purchase"
-
-# insert lemons
+INSERT_INVENTORY(){
+    # insert lemons
 # get contents from csv file | replace /r(carriage returns) with emptylines
 cat product.csv | sed -e 's/\r//g' | while IFS="," read LEMONS
 do
@@ -38,11 +21,67 @@ do
                 if [[ $INSERT_LEMONS == "INSERT 0 1" ]]
                 then
                     echo "$LEMONS inserted into inventory"
-                fi
-        fi     
+                fi 
+        fi      
 done
 }
+MENU(){
+# get num of rows
+MAX_ROWS=$($PSQL "select count(lemons) from product")
+if [[ ! $MAX_ROWS -gt 1 ]]
+then
+    INSERT_INVENTORY
+    MENU
 
+else
+
+if [[ $1 ]]
+then echo -e "\n$1"
+fi
+echo -e "\nMain Menu\n"
+echo -e "1)Purchase lemons\n2)view our sales\n3)exit"
+read OPTION
+
+case $OPTION in 
+1) PURCHASE ;;
+2) VIEW_OUR_SALES ;;
+3) EXIT ;;
+esac
+
+fi
+}
+
+PURCHASE(){
+echo -e "Welcome to Purchase"
+# What is your name?
+sleep 1
+                echo -e "\nWhat is your name? (first name)"
+                read CUSTOMER_NAME
+                # if customer name includes anything except letters
+                if [[ ! $CUSTOMER_NAME =~ ^[a-zA-Z]+$ ]]
+                then
+                # Enter a valid name with letters
+                MENU "Enter a valid name with letters."
+                else
+                        INSERT_CUSTOEMR_NAME=$($PSQL "insert into customers(name) values('$CUSTOMER_NAME')")
+                fi
+                # Choose available lemons to purchase
+                echo -e "\nHi, $CUSTOMER_NAME\nPick your lemons."
+                    sleep 2
+                AVAILABLE_LEMONS=$($PSQL "select * from product")
+                # echo $($AVAILABLE_LEMONS)
+                echo $AVAILABLE_LEMONS | sed -E 's/\|//g' | sed -E 's/t//g' | sed -E 's/   ([a-zA-Z]+)/\1\n/g' | sed -E "s/^ //g"
+                read OPTION
+                # if option is invalid
+                LEMON_SELECTED=$($PSQL "select lemons from product where product_id=$OPTION")
+                if [[ !$LEMON_SELECTED =~ ^[0-9]{,2}$ ]]
+                then
+                    echo -e "\n This is not a valid selection"
+                else
+                    echo -e "\n$CUSTOMER_NAME selected$LEMON_SELECTED lemon-batch"
+                fi
+
+}
 
 VIEW_OUR_SALES(){
 echo "View our sales"
